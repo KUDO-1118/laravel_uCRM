88db0b0d545093e1ab3e8dc2cd2425db89389958
{KUDO} <au4saf010@gmail.com>
2025-06-14 11:14:39 +0900

    フォーム(create)

--- app/Http/Controllers/InertiaTestController.php
+++ app/Http/Controllers/InertiaTestController.php
@@ -14,6 +14,11 @@ class InertiaTestController extends Controller
         return Inertia::render('Inertia/Index');
     }
 
+    public function create()
+    {
+        return Inertia::render('Inertia/Create');
+    }
+
     public function show($id)
     {
         // dd($id);
