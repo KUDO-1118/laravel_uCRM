397643f9305587f88086fc5f701febd5762a4e36
{KUDO} <au4saf010@gmail.com>
2025-07-07 17:41:30 +0900

    顧客情報をv-forで表示

--- resources/js/Pages/Purchases/Create.vue
+++ resources/js/Pages/Purchases/Create.vue
@@ -2,14 +2,26 @@
 import { getToday } from '@/commom';
 import { onMounted, reactive } from 'vue';
 
+const props = defineProps({ 'customers': Array })
+
 onMounted(() => { //ページ読み込み後 即座に実行
   form.date = getToday()
 })
 
-const form = reactive({ date: null })
+const form = reactive({
+  date: null,
+  customer_id: null
+ })
+
 </script>
 
 <template>
   日付<br>
-  <input type="date" name="date" v-model="form.date">
+  <input type="date" name="date" v-model="form.date"><br>
+  会員名<br>
+  <select name="customer" v-model="form.customer_id">
+    <option v-for="customer in customers" :value="customer_id" :key="customer.id">
+      {{ customer.id }} : {{ customer.name }}
+    </option>
+  </select>
 </template>
